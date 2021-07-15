4d6d687281dead49a74dd58c7cb9dfe9fd0ac936
nikhilmwarrier <champion.26nikhil@gmail.com>
2021-07-15 17:54:13 +0530

    add basic autocomplete for braces etc. Messes undo-redo stack

--- Changelog.md
+++ Changelog.md
@@ -1,9 +1,22 @@
 # Changelog
 
+### 15 Jul 2021
+
+Morning:
+
+- Tried CodeJar. Didn't work well.
+- Create custom editor with syntax highlighting
+
+Evening:
+
+- Add tab support
+- Implement basic autocompletion for quotes, brackets, braces, etc
+- Somehow autocomplete breaks browser's undo-redo stack. Gotta fix that.
+
 ### 14 Jul 2021
 
 - Started project
 - Installed `react-router-dom`
 - Looking for a better autocomplete engine for Ace Editor. Kidna like the one SPCK uses
 
-<!-- TODO: find good editor with autocomplete -->
+<!-- TODO: fix undo-redo stack -->
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -3,9 +3,18 @@ import "./styles/TextEditor.css";
 
 import Prism from "prismjs";
 
-function TextEditor() {
-	const highlight_div = useRef(null);
+function TextEditor(props) {
+	const [config, setConfig] = useState(null);
 	const [value, setValue] = useState("* { color: 'red'; }");
+
+	const defaultConfig = {
+		tabsEnabled: true,
+		autocompleteBraces: true,
+		autocompleteBrackets: true,
+		autocompleteQuotes: true,
+	};
+
+	const highlight_div = useRef(null);
 
 	const handleChange = e => {
 		if (e.target.value === value) return;
@@ -13,15 +22,56 @@ function TextEditor() {
 		console.log(highlight_div.current);
 	};
 
+	const handleKeydown = e => {
+		const el = e.target;
+		const start = el.selectionStart;
+		const end = el.selectionEnd;
+		const tabChar = "\t";
+
+		const autocomplete = (char, charToAdd) => {
+			if (e.key !== char) return el.value;
+			e.preventDefault();
+			el.value =
+				el.value.substring(0, start) +
+				char +
+				charToAdd +
+				el.value.substring(end);
+			el.selectionStart = el.selectionEnd = start + char.length;
+			return el.value;
+		};
+
+		if (config.tabsEnabled === true && e.key === "Tab") {
+			e.preventDefault();
+			el.value =
+				el.value.substring(0, start) + tabChar + el.value.substring(end);
+			setValue(el.value);
+			el.selectionStart = el.selectionEnd = start + 1;
+		}
+		if (config.autocompleteBraces === true) setValue(autocomplete("{", "}"));
+		if (config.autocompleteBrackets === true) setValue(autocomplete("(", ")"));
+		if (config.autocompleteQuotes === true) {
+			setValue(autocomplete("'", "'"));
+			setValue(autocomplete('"', '"'));
+		}
+	};
+
+	useEffect(() => {
+		if (!props.config) setConfig(defaultConfig);
+		else setConfig(props.config);
+		return () => setConfig(null);
+	}, []);
+
 	useEffect(() => {
 		if (!highlight_div.current) return;
 		Prism.highlightElement(highlight_div.current);
 	}, [value]);
+
 	return (
 		<div className="texteditor__parent">
 			<textarea
 				className="texteditor__textarea"
 				onChange={handleChange}
+				onKeyDown={handleKeydown}
 				value={value}></textarea>
 			<div
 				ref={highlight_div}
